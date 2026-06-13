419eccccd840749fd001cd4b215956a0b969bd4b
Roshini <roshmani56@gmail.com>
2026-06-13 10:05:35 +0400

    locators

--- src/test/java/XpathLocators.java
+++ src/test/java/XpathLocators.java
@@ -3,14 +3,44 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class XpathLocators {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 //        LocateElementsByXpath(driver);
 //        Loginfunctionality(driver);
-        LocateElementsByPredicates(driver);
+//        LocateElementsByPredicates(driver);
+//        LocateElementsByAxes(driver);
+        LocateElementsByXathFunctions(driver);
+        LocateElementsByOperators(driver);
+    }
 
+    private static void LocateElementsByOperators(WebDriver driver) {
+        Loginfunctionality(driver);
+        driver.findElement(By.xpath("//a[contains(@href, 'linkedin') or text() = 'LinkedIn']")).click();
+        driver.findElement(By.xpath("//button[contains(@class,'btn_inventory ') and contains(@id,'onesie') ]")).click();
+    }
+
+    private static void LocateElementsByXathFunctions(WebDriver driver) {
+        Loginfunctionality(driver);
+//        List<WebElement> priceList = driver.findElements(By.xpath("//div[contains(@class, 'item_price')]"));
+//        for (WebElement price : priceList) {
+//            System.out.println(price.getText());
+//        }
+        driver.findElement(By.xpath("//div[text() = 'Sauce Labs Fleece Jacket']/following::button[1]")).click();
+        driver.findElement(By.xpath("//img[starts-with(@alt,'Test')]/following::button")).click();
+    }
+
+    private static void LocateElementsByAxes(WebDriver driver) {
+        driver.get("https://www.w3schools.com/html/html_tables.asp");
+        System.out.println(driver.findElement(By.xpath("//table[@id = 'customers']//child::tr[5]/child::td[2]")).getText());
+        System.out.println(driver.findElement(By.xpath("//table[@id = 'customers']/descendant::th[2]")).getText());
+        System.out.println(driver.findElement(By.xpath("//table[@id = 'customers']/descendant::th[2]/parent::tr")).getTagName());
+        System.out.println(driver.findElement(By.xpath("//table[@id = 'customers']//child::tr[5]/child::td[2]/ancestor::table")).getAttribute("class"));
+        System.out.println(driver.findElement(By.xpath("//table[@id = 'customers']//child::tr[5]/child::td[2]/preceding-sibling::td")).getText());
+        System.out.println(driver.findElement(By.xpath("//table[@id = 'customers']//child::tr[5]/child::td[2]/following-sibling::td")).getText());
     }
 
     private static void Loginfunctionality(WebDriver driver) {
